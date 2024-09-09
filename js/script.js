18ba3a80066f00f9f8e82acc193bff274ac4bc92
minnatur-rahman <minnatur7373@gmail.com>
2024-09-10 06:08:32 +0800

    ashdfah

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const MyBtn = document.querySelector(".MyBtn button");
-const RoolsBox = document.querySelector("RoolsBox");
+const RoolsBox = document.querySelector(".RoolsBox");
+const exitButton = document.querySelector(".Buttons .ExitButton");
 
 
 
  MyBtn.onclick = () => {
     RoolsBox.classList.add("activeInfo");
  } 
+
+ exitButton.onclick = () => {
+   RoolsBox.classList.remove("activeInfo");
+} 
