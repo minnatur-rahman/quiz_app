8f15c2cd36e14d0ed4680392af1dd8ce61a5fbbf
minnatur-rahman <minnatur7373@gmail.com>
2024-09-10 21:50:50 +0800

    asdfh

--- js/question.js
+++ js/question.js
@@ -2,6 +2,24 @@ let question = [
     {
         num:1,
         question:'What Dose HTML Stand for?',
-        answer:
-    }
+        answer:'Hyper Text Markup Language',
+        option:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        num:2,
+        question:'What Dose CSS Stand for?',
+        answer:'Cascading Style Sheet',
+        option:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+
 ]
